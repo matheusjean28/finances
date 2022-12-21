00e9c08f0b138868675053313cbd33ec15357755
Dev_matth <45796720+matheusjean28@users.noreply.github.com>
2022-12-21 17:26:47 -0300

    Modificações na rota POST
    Adicionado biblioteca nativa do node para ID

--- user_check/user_check.js
+++ user_check/user_check.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const express = require('express')
 const router = express.Router()
 
@@ -21,20 +22,6 @@ let counts = [{
 },
 {
     "id": "testedois",
-    "nome": "123",
-    "preco": "1654",
-    "data": "2022-11-26",
-    "status": "true"
-},
-{
-    "id": "testedois",
-    "nome": "123",
-    "preco": "1654",
-    "data": "2022-11-26",
-    "status": "true"
-},
-{
-    "id": "1",
     "nome": "123",
     "preco": "1654",
     "data": "2022-11-26",
@@ -68,15 +55,25 @@ function check_user_id(req, res, next) {
 
 //criar uma nova conta na lista de contas
 function create_pagament(req, res, next) {
-    let item = req.body;
 
-    let teste = Math.random(1 * 100) 
+    let nome = req.body.nome;
+    let preco = req.body.preco;
+    let data = req.body.data;
+    let status = req.body.status;
+    let id = crypto.randomBytes(16).toString("hex");
+
+    /* 
+    adicionar validação para tamanhos dos campos
+    se o tamanho e o formato do campo for valido
+    adicionar o mesmo no contexto de payment para proseguir para a proxima tarefa
+    */ 
 
 
-
+    let payment = {nome, preco, data, status, id}
+    console.log('este é o paymenbte',payment)
 
     //colocar o item dentro do counts, que é a lista de contas ficcticia
-    counts.push(item)
+    counts.push(payment)
     next()
 }
 
@@ -115,10 +112,9 @@ router.get('/mycount', check_user_id, (req, res) => {
 })
 
 
-router.post('/', create_pagament, (req, res) => {
-
-    //res.json(counts)
-    res.json()
+router.post('/create', create_pagament, (req, res) => {
+    /*responder com o ultimo pagamento adicionado */
+    res.json(counts)
 })
 
 
